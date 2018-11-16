8cab2a70abb22f224dfe40ecd7c66232c758e17c
Didin Abu Aisy <didin@myworkforce.com>
2018-11-16 09:36:17 +0700

    Add Contact, Login and Registration children components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Login from './Login';
+import Registration from './Registration';
+import Contact from './Contact';
+
 class App extends Component {
+
+  onMenuClicked (idx) {
+    this.child[idx].setAction();
+    this.setState({active:idx});
+  }
+
+  changeAction (actionOptions) {
+    this.setState({
+      action: actionOptions
+    });
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      active:0,
+      items: [
+        { text: "Login"},
+        { text: "Registration"},
+        { text: "Contact"}
+      ],
+      action: {
+        cancel: {
+          text: "Cancel"
+        },
+        submit: {
+          text: "Submit"
+        }
+      }
+    }
+    this.child = [];
+    this.changeAction  = this.changeAction.bind(this);
+    this.onMenuClicked = this.onMenuClicked.bind(this);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+        <h2>Didin Application</h2>        
+        {
+          this.state.items.map((item, idx) => (
+            <button onClick={(e) => {
+              this.onMenuClicked(idx);
+              e.preventDefault();
+            }}>{item.text}</button>
+          ))
+        }    
+        
+        <section>
+            <Login show={this.state.active==0}  changeHandler={ this.changeAction } ref={instance => this.child[0] = instance} />
+            <Registration show={this.state.active==1} changeHandler={ this.changeAction } ref={instance => this.child[1] = instance} />  
+            <Contact show={this.state.active==2}  changeHandler={ this.changeAction } ref={instance => this.child[2] = instance} />            
+        </section>
+        <div>
+          <button>{this.state.action.cancel.text}</button>
+          <button>{this.state.action.submit.text}</button>
+        </div>
+      </div>
       </div>
     );
   }
